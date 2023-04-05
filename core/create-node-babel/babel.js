5aa628c9e9a7dc8a859099bd267d6ade7fbdf0b1
stack-wuh <wuh131420@foxmail.com>
2023-04-05 17:44:42 +0800

    ast(babel): 利用babel新增注释类型节点

---
+++ core/create-node-babel/babel.js
@@ -0,0 +1,59 @@
+import fsp from 'fs/promises'
+import parser from '@babel/parser'
+import traverse from '@babel/traverse'
+import core from '@babel/core'
+import prettier from 'prettier'
+
+const filepath = './demo.js'
+
+
+const workflowBabel = async () => {
+  const source = await fsp.readFile(filepath, { encoding: 'utf-8' })
+
+  const ast = parser.parse(source)
+
+  traverse.default(ast, {
+    // 这里不对，这个是在var后面加了注释
+    Identifier (path) {
+      if (path.node.name === 'str') {
+        path.node.name = 'str'
+        path.parentPath.parentPath.addComment('leading', '这里是一条注释', true)
+      }
+    },
+
+    /**
+     * @NOTE 可以用声明表达式去找对应的节点
+     * 
+     * !1. 现在是有一个前置条件, 只给num1这个变量加前后注释
+     */
+    VariableDeclarator (path) {
+      if (path.node.id.name === 'num1') {
+        // console.log('=====> variableDeclaration.path', path)
+        /**
+         * @NOTE false代表是多行注释，true 代表单行注释
+         */
+        path.parentPath.addComment('leading', ' before \n *asd \n *sss \n', false)
+        path.addComment('trailing', 'after', true)
+      }
+    },
+    VariableDeclaration (path) {
+      const [head] = path.node.declarations
+
+
+      path.addComment('trailing', `${head.id.name} 的后置注释`, false)
+      path.addComment('leading', `${head.id.name} 的前置注释`, false)
+    }
+  })
+
+  const result = await core.transformFromAstSync(ast, source, {
+    generatorOpts: {
+      minified: false,
+      comments: true,
+    }
+  })
+  console.log('======> result', prettier.format(result.code, { semi: false, quotes: 'double' }))
+
+  // console.log('=====> source', ast)
+}
+
+workflowBabel()
---
+++ core/create-node-babel/demo.js
@@ -0,0 +1,17 @@
+var num1 = 10
+
+var str = 'str1'
+
+var date1 = '2023-04-01'
+
+var arr1 = ['1', 2, 3, null, undefined]
+
+var func1 = function () {
+  console.log('=====> func1')
+}
+
+var func2 = async function () {
+  console.log('====> func2')
+}
+
+var regExp = new RegExp('\d+', 'gi')
